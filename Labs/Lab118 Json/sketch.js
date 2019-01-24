bca2dd113c88154a4497d170dd3f7b33abb28e36
796291 <43755477+796291@users.noreply.github.com>
2019-01-24 08:38:28 -0800

    json

--- Labs/Lab118 Json/sketch.js
+++ Labs/Lab118 Json/sketch.js
@@ -3,32 +3,32 @@
 //Json Sort
 
 var data = [];
+
 function preload(){
   data = loadJSON("data.json");
+}
 
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
-
 }
 
 function draw() {
-
 }
 
 function organize(){
   var temp;
-  for (var i = 1; i < data.length; i++){
+  for (var i = 1; i < data.countrydata.length; i++){
     for(var j = i; j > 0; j--){
-      if(data[j] < data[j-1]){
-        temp = data[j];
-        data[j] = data[j-1];
-        data[j-1] = temp;
+      if(data.countrydata[j].total < data.countrydata[j-1].total){
+        temp = data.countrydata[j];
+        data.countrydata[j] = data.countrydata[j-1];
+        data.countrydata[j-1] = temp;
         swaps++;
       }
     }
   }
+  console.log(data);
 }
