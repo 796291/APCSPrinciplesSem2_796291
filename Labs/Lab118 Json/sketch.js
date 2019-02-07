07f7da3e36e5482c5c799973bebe1a5e00cdeb2b
796291 <43755477+796291@users.noreply.github.com>
2019-02-07 08:46:08 -0800

    GRAPH

--- Labs/Lab118 Json/sketch.js
+++ Labs/Lab118 Json/sketch.js
@@ -4,6 +4,7 @@
 
 var data = [];
 var sortBy = "males";
+var bigTotal = 0;
 
 function preload(){
   data = loadJSON("population.json");
@@ -15,6 +16,7 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
   bubble();
+  represent();
 }
 
 function draw() {
@@ -36,7 +38,7 @@ function bubble(){
   if(sortBy = "females"){
     for (var i = 1; i < data.countrydata.length; i++){
       for(var j = i; j > 0; j--){
-        if(data.countrydata[j].total < data.countrydata[j-1].total){
+        if(data.countrydata[j].females < data.countrydata[j-1].females){
           temp = data.countrydata[j];
           data.countrydata[j] = data.countrydata[j-1];
           data.countrydata[j-1] = temp;
@@ -47,7 +49,7 @@ function bubble(){
   if(sortBy = "males"){
     for (var i = 1; i < data.countrydata.length; i++){
       for(var j = i; j > 0; j--){
-        if(data.countrydata[j].total < data.countrydata[j-1].total){
+        if(data.countrydata[j].males < data.countrydata[j-1].males){
           temp = data.countrydata[j];
           data.countrydata[j] = data.countrydata[j-1];
           data.countrydata[j-1] = temp;
@@ -56,4 +58,17 @@ function bubble(){
     }
   }
   console.log(data);
+}
+
+function represent(){
+  for(var i = 1; i < data.countrydata.length; i++){
+    bigTotal = bigTotal + data.countrydata[i].total;
+  }
+  var lastAngle = 0;
+  for(var i = 0; i < data.countrydata.length; i ++){
+    var angle = ((data.countrydata[i].total / bigTotal) * (2 * PI));
+    fill(0, 0, 10*i);
+    arc(300, 300, 500, 500, lastAngle, lastAngle + angle);
+    lastAngle += angle
+  }
 }
