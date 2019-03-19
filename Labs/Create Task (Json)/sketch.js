dc97a0cf4574c577e3d10319f3d5a395e9a3e7b6
796291 <43755477+796291@users.noreply.github.com>
2019-03-19 08:49:30 -0700

    governer data
    sorting works: add options on how to sort

--- Labs/Create Task (Json)/sketch.js
+++ Labs/Create Task (Json)/sketch.js
@@ -1,9 +1,10 @@
 //Jakob Hachigian-Kreutzer
-//Create Task
-var sort = "country";
+//2/26/19
+//Json Sort
+var sortname = "state_name";
 var data = [];
 function preload(){
-  data = loadJSON("population.json");
+  data = loadJSON("governers.json");
 }
 
 
@@ -15,11 +16,7 @@ function setup() {
   //text
   textSize(10);
   textAlign(CENTER, CENTER);
-  //directions
-  console.log("c: sort by country")
-  console.log("t: sort by total")
-  console.log("m: sort by males")
-  console.log("f: sort by females")
+  organize(sortname);
 }
 
 
@@ -27,91 +24,35 @@ function draw() {
   //redraw background
   background(5, 5, 5);
   //calls
-  organize(sort);
+  //organize(sortname);
 }
 
 
 function organize(sortby){
   var temp;
-  for (var i = 1; i < data.countrydata.length; i++){
+  for (var i = 1; i < data.length; i++){
     for(var j = i; j > 0; j--){
       //add change sorting by command
-      if(data.countrydata[j][sortby] < data.countrydata[j-1][sortby]){
-        temp = data.countrydata[j];
-        data.countrydata[j] = data.countrydata[j-1];
-        data.countrydata[j-1] = temp;
+      if(data[j][sortby] < data[j-1][sortby]){
+        temp = data[j];
+        data[j] = data[j-1];
+        data[j-1] = temp;
       }
     }
   }
   drawWords();
-  //check();
-  bars();
+  check();
 }
 
 
 function drawWords(){
   textAlign(LEFT);
-  //titles
-  fill(255, 255, 255);
-  text("Countries", 100, 10);
-  text("Totals", 300, 10);
-  text("Males", 500, 10);
-  text("Females", 700, 10);
-  //names
-  fill(255, 0, 0);
-  for(var i = 1; i < data.countrydata.length; i++){
-    text(data.countrydata[i].country, 100, 10 + (i*10));
-  }
-  //total
-  fill(0, 255, 0);
-  for(var i = 1; i < data.countrydata.length; i++){
-    text(data.countrydata[i].total, 300, 10 + (i*10));
-  }
-  //male
-  fill(0, 0, 255);
-  for(var i = 1; i < data.countrydata.length; i++){
-    text(data.countrydata[i].males, 500, 10 + (i*10));
-  }
-  //female
-  fill(0, 0, 255);
-  for(var i = 1; i < data.countrydata.length; i++){
-    text(data.countrydata[i].females, 700, 10 + (i*10));
-  }
 }
 
 
 function check(){
-  for(var i = 1; i < data.countrydata.length; i++){
-    console.log(data.countrydata[i].total);
-  }
-}
-
-function bars(){
-  var allTotal = 0;
-  var allMale = 0;
-  var allFemale = 0;
-  for(var i = 1; i < data.countrydata.length; i++){
-    allTotal = allTotal + data.countrydata[i].total;
-    allMale = allMale + data.countrydata[i].males;
-    allFemale = allFemale + data.countrydata[i].females;
-  }
-  //total
-  for(var i = 1; i < data.countrydata.length; i++){
-    var size = (data.countrydata[i].total / allTotal) * (100);
-    fill(0, 255, 0);
-    rect(350, 10 + (i*10), size*5, 10);
-  }
-  //male
-  for(var i = 1; i < data.countrydata.length; i++){
-    var size = (data.countrydata[i].males / allMale) * (100);
-    fill(0, 0, 255);
-    rect(550, 10 + (i*10), size*5, 10);
-  }
-  //female
-  for(var i = 1; i < data.countrydata.length; i++){
-    var size = (data.countrydata[i].females / allFemale) * (100);
-    fill(0, 0, 255);
-    rect(750, 10 + (i*10), size*5, 10);
+  for(var i = 1; i < data.length; i++){
+    console.log(data[i].name);
   }
 }
 
@@ -119,18 +60,18 @@ function bars(){
 function keyPressed(){
   //c; country
   if(keyCode === 67){
-    sort = "country";
+    sortname = "country";
   }
   //t; total
   if(keyCode === 84){
-    sort = "total";
+    sortname = "total";
   }
   //m; male
   if(keyCode === 77){
-    sort = "males";
+    sortname = "males";
   }
   //f; female
   if(keyCode === 70){
-    sort = "females";
+    sortname = "females";
   }
 }
